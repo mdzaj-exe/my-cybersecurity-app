6849c75042968b3d21fbea8fcfd352dceaf8c213
mdzaj-exe <ajwint.cs@gmail.com>
2024-10-02 01:32:22 -0400

    Refactor CSS styles for the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "../stylesheets/Header.css";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
