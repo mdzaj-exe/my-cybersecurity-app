3122fa0de7aaaf64c8a6793214e864378d8f113b
Ahmad Wint <ajwint.cs@gmail.com>
2024-11-01 17:30:43 -0400

    Added Education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import "../stylesheets/Education.css";
 
 const Education = () => (
   <section className="education">
     <h2>Education</h2>
-    <p>Details about your educational background.</p>
+
+    <div className="education-item">
+      <h3>Western Governors University</h3>
+      <p>Bachelor of Science - BS, Computer Software Engineering</p>
+      <p>November 2023 - October 2024</p>
+    </div>
+
+    <div className="education-item">
+      <h3>Occidental College</h3>
+      <p>Computer Science, Computer Programming</p>
+      <p>August 2020 - May 2023</p>
+    </div>
   </section>
 );
 
