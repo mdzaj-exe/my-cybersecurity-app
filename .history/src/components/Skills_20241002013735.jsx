e2785b0bd31fe3089e06dfbb611e8919b609f8b2
Ahmad Wint <ajwint.cs@gmail.com>
2024-11-01 17:05:37 -0400

    Added Skills/Certifications section

---
+++ .history/src/components/Skills_20241002013735.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../stylesheets/Skills.css";
+
+const Skills = () => (
+  <section className="skills">
+    <h2>Skills & Certifications</h2>
+    <div className="grid-container">
+      <div className="grid-item">JavaScript</div>
+      <div className="grid-item">React</div>
+      <div className="grid-item">AWS Certified Cloud Practitioner</div>
+      {/* Add more skills and certifications */}
+    </div>
+  </section>
+);
+
+export default Skills;
