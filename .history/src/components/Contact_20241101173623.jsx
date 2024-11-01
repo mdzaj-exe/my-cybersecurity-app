c5456a34d1d73f886ef7d38d2503078dd2edf91e
Ahmad Wint <ajwint.cs@gmail.com>
2024-11-01 17:49:33 -0400

    Updated Project component

--- .history/src/components/Contact_20241101173623.jsx
+++ .history/src/components/Contact_20241101173623.jsx
@@ -5,15 +5,8 @@ const Contact = () => (
   <section className="contact">
     <h2>Contact Me</h2>
     <div className="scrolling-links">
-      <a href="https://www.linkedin.com/in/aj-in-cs/" target="_blank">
-        LinkedIn
-      </a>
-      <a
-        href="https://github.com/mdzaj-exe/cybersecurity-portfolio"
-        target="_blank"
-      >
-        GitHub
-      </a>
+      <a href="https://www.linkedin.com/in/aj-in-cs/">LinkedIn</a>
+      <a href="https://github.com/mdzaj-exe/cybersecurity-portfolio">GitHub</a>
       {/* Add more links as needed */}
     </div>
   </section>
---
+++ .history/src/components/Education_20241101173455.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../stylesheets/Education.css";
+
+const Education = () => (
+  <section className="education">
+    <h2>Education</h2>
+
+    <div className="education-item">
+      <h3>Western Governors University</h3>
+      <p>Bachelor of Science - BS, Computer Software Engineering</p>
+      <p>November 2023 - October 2024</p>
+    </div>
+
+    <div className="education-item">
+      <h3>Occidental College</h3>
+      <p>Computer Science, Computer Programming</p>
+      <p>August 2020 - May 2023</p>
+    </div>
+  </section>
+);
+
+export default Education;
